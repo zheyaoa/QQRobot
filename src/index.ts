b56c3d5216c3930823941543da6f5d020f79465d
zheyaoa <1578127412@qq.com>
2019-09-25 18:41:38 +0800

    parse post info

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ import * as Koa from 'koa';
 const app:Koa = new Koa();
 
 app.use(async (ctx)=>{
-    let url:string = ctx.url;
-    console.log(url)
+    ctx.body = "hello koa2";    
+    let data:string = '';
+    ctx.req.addListener('data',(chunk) => {
+        data+=chunk;
+    })
+    ctx.req.addListener('end',() => {
+        console.log(data)
+	})
+	console.log("aabbcc")
 })
 app.listen(3000,function(){
     console.log("server run on port 3000")
